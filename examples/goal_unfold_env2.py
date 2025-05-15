062b6e617210749b65ca4e9ff33ae7912c837229
zzqingY <zzq0551@foxmail.com>
2025-05-15 22:25:36 +0800

    [M] path to saving videos

--- examples/goal_unfold_env2.py
+++ examples/goal_unfold_env2.py
@@ -181,7 +181,7 @@ def main():
     parser.add_argument('--env_name', type=str, default='ClothFlattenPPP')
     parser.add_argument('--headless', type=int, default=0, help='Whether to run the environment with headless rendering')
     parser.add_argument('--num_variations', type=int, default=1, help='Number of environment variations to be generated')
-    parser.add_argument('--save_video_dir', type=str, default='./data/', help='Path to the saved video')
+    parser.add_argument('--save_video_dir', type=str, default='./video/', help='Path to the saved video')
     parser.add_argument('--img_size', type=int, default=720, help='Size of the recorded videos')
     parser.add_argument('--test_depth', type=int, default=0, help='If to test the depth rendering by showing it')
 
